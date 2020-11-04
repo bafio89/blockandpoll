6d1fa326e9c1d7a981fa38a5a7f3573efc8048ed
ffiorella <fabio.fiorella@lastminute.com>
2020-11-04 23:11:06 +0100

    start to create callble transaction

--- README.md
+++ README.md
@@ -2,3 +2,6 @@ DEPLOYARE su HEROKU
 
 (installare plugin nel caso il comando deploy non sia riconosciuto : heroku plugins:install java)
 usare comando: heroku deploy:jar target/pollAlgorand-0.0.1-SNAPSHOT.jar --app pollalgorand
+
+
+link utile di algorand per callableTransaction https://developer.algorand.org/docs/features/asc1/stateful/sdks/
--- src/test/java/com/pollalgorand/rest/AlgorandBlockchainPollRepositoryTest.java
+++ src/test/java/com/pollalgorand/rest/AlgorandBlockchainPollRepositoryTest.java
@@ -1,6 +1,10 @@
 package com.pollalgorand.rest;
 
+import com.algorand.algosdk.account.Account;
 import com.algorand.algosdk.builder.transaction.ApplicationCallTransactionBuilder;
+import com.algorand.algosdk.crypto.Address;
+import com.algorand.algosdk.crypto.TEALProgram;
+import com.algorand.algosdk.logic.StateSchema;
 import com.algorand.algosdk.transaction.Transaction;
 import com.algorand.algosdk.v2.client.common.AlgodClient;
 import org.junit.Test;
@@ -14,8 +18,21 @@ public class AlgorandBlockchainPollRepositoryTest {
 
 
   @Test
-  public void submitATransaction() {
+  public void submitATransaction() throws Exception {
 
+
+    TEALProgram tealProgram = new TEALProgram();
+
+    Account creatorAccount = new Account("a mnemonic string");
+    Address sender = creatorAccount.getAddress();
+    Transaction txn = Transaction.ApplicationCreateTransactionBuilder()
+        .sender(sender)
+        .suggestedParams(algodClient.TransactionParams().execute().body())
+        .approvalProgram(tealProgram)
+        .clearStateProgram(null)
+        .globalStateSchema(new StateSchema(1, 0))
+        .localStateSchema(new StateSchema(1, 1))
+        .build();
 //    Transaction applicationCallTransaction = Transaction.ApplicationCreateTransactionBuilder().approvalProgram().build();
 
   }
