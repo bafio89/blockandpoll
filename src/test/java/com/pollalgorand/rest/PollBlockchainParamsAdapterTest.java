ebfa090e4f52249c4247d9acd82ccd9b37e399cc
ffiorella <fabio.fiorella@lastminute.com>
2020-11-10 21:06:42 +0100

    refactoring

--- src/test/java/com/pollalgorand/rest/PollBlockchainParamsAdapterTest.java
+++ src/test/java/com/pollalgorand/rest/PollBlockchainParamsAdapterTest.java
@@ -12,13 +12,13 @@ import org.junit.Test;
 
 public class PollBlockchainParamsAdapterTest {
 
+  public static final String A_POLL = "A_POLL";
   private PollBlockchainParamsAdapter pollBlockchainParamsAdapter;
+  public static final String SENDER = "A_SENDER";
 
   @Test
   public void adaptToPollTealParams() {
 
-    String aPoll = "A_POLL";
-    String sender = "A_SENDER";
     Date startVotingTime = new Date();
     Date endVotingTime = new Date();
     Date startSubscriptionTime = new Date();
@@ -27,15 +27,15 @@ public class PollBlockchainParamsAdapterTest {
 
     List<byte[]> optionsInByte = asList("Option1".getBytes(UTF_8), "Option2".getBytes(UTF_8));
 
-    PollTealParams expectedTealParams = new PollTealParams(aPoll.getBytes(UTF_8), startVotingTime,
+    PollTealParams expectedTealParams = new PollTealParams(A_POLL.getBytes(UTF_8), startVotingTime,
         endVotingTime, startSubscriptionTime, endSubscriptionTime, optionsInByte,
-        sender.getBytes(UTF_8));
+        SENDER.getBytes(UTF_8));
 
     pollBlockchainParamsAdapter = new PollBlockchainParamsAdapter();
 
     Poll poll = new Poll(
-        aPoll, startVotingTime, endVotingTime, startSubscriptionTime,
-        endSubscriptionTime, options, sender);
+        A_POLL, startVotingTime, endVotingTime, startSubscriptionTime,
+        endSubscriptionTime, options, SENDER);
 
     PollTealParams pollTealParams = pollBlockchainParamsAdapter.fromPollToPollTealParams(poll);
 
