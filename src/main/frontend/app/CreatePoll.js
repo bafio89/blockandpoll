24c7c4e295c630e02b2432fccb0fcd1a79265fc1
ffiorella <fabio.fiorella@lastminute.com>
2020-12-07 21:01:54 +0100

    now we have associate events to our form fields

--- src/main/frontend/app/CreatePoll.js
+++ src/main/frontend/app/CreatePoll.js
@@ -4,18 +4,85 @@ import Button from "@material-ui/core/Button";
 
 class CreatePoll extends React.Component{
 
+  constructor(props) {
+    super(props);
+    this.state = {sender: ''}
+    this.state = {mnemonicKey: ''}
+    this.state = {name: ''}
+    this.state = {option1: ''}
+    this.state = {option2: ''}
+    this.state = {startSubDate: ''}
+    this.state = {endSubDate: ''}
+    this.state = {startVotingDate: ''}
+    this.state = {endVotingDate: ''}
+
+    this.handleSenderChange = this.handleSenderChange.bind(this);
+    this.handleMnemonicKeyChange = this.handleMnemonicKeyChange.bind(this);
+    this.handleNameChange = this.handleNameChange.bind(this);
+    this.handleOption1Change = this.handleOption1Change.bind(this);
+    this.handleOption2Change = this.handleOption2Change.bind(this);
+    this.handleStartSubDateChange = this.handleStartSubDateChange.bind(this);
+    this.handleEndSubDateChange = this.handleEndSubDateChange.bind(this);
+    this.handleStartVotingDateChange = this.handleStartVotingDateChange.bind(this);
+    this.handleEndVotingDateChange = this.handleEndVotingDateChange.bind(this);
+
+    this.createPoll = this.createPoll.bind(this);
+  }
+
+  handleSenderChange(event) {
+    console.log(event.target.value)
+    this.setState({sender: event.target.value})
+  }
+
+  handleMnemonicKeyChange(event){
+    this.setState({mnemonicKey: event.target.value})
+  }
+
+  handleNameChange(event){
+    this.setState({name: event.target.value})
+  }
+
+  handleOption1Change(event){
+    this.setState({option1: event.target.value})
+  }
+
+  handleOption2Change(event){
+    this.setState({option2: event.target.value})
+  }
+
+  handleStartSubDateChange(event){
+    this.setState({startSubDate: event.target.value})
+  }
+
+  handleEndSubDateChange(event){
+    this.setState({endSubDate: event.target.value})
+  }
+
+  handleStartVotingDateChange(event){
+    this.setState({startVotingDate: event.target.value})
+  }
+
+  handleEndVotingDateChange(event){
+    console.log(event.target.value)
+    this.setState({endVotingDate: event.target.value})
+  }
+
+  createPoll(event) {
+    console.log("babab " + this.state.sender);
+  }
+
   render() {
     return (
         <div>
-          <form className="createPoll" noValidate autoComplete="off" onSubmit={this.createPoll()}>
-            <TextField id="sender" label="Creator address" variant="outlined" />
-            <TextField id="mnemonicKey" label="Passphrase" variant="outlined" />
+          <form className="createPoll" noValidate autoComplete="off" onSubmit={this.createPoll}>
+            <TextField id="sender" label="Creator address" variant="outlined" value={this.state.sender || ''} onChange={this.handleSenderChange}/>
+            <TextField id="mnemonicKey" label="Passphrase" variant="outlined" value={this.state.mnemonicKey || ''} onChange={this.handleMnemonicKeyChange}/>
             <br/>
-            <TextField id="pollName" label="Name" variant="outlined" />
+            <TextField id="pollName" label="Name" variant="outlined" value={this.state.name || ''} onChange={this.handleNameChange}/>
             <br/>
-            <TextField id="option1" label="Option 1" variant="outlined" />
+            <TextField id="option1" label="Option 1" variant="outlined" value={this.state.option1 || ''} onChange={this.handleOption1Change}/>
             <br/>
-            <TextField id="option2" label="Option 2" variant="outlined" />
+            <TextField id="option2" label="Option 2" variant="outlined" value={this.state.option2 || ''} onChange={this.handleOption2Change}/>
             <br/>
             <TextField
                 id="date"
@@ -26,6 +93,8 @@ class CreatePoll extends React.Component{
                 InputLabelProps={{
                   shrink: true,
                 }}
+                value={this.state.startSubDate || ''}
+                onChange={this.handleStartSubDateChange}
             />
             <TextField
                 id="date"
@@ -36,6 +105,8 @@ class CreatePoll extends React.Component{
                 InputLabelProps={{
                   shrink: true,
                 }}
+                value={this.state.endSubDate || ''}
+                onChange={this.handleEndSubDateChange}
             />
             <TextField
                 id="date"
@@ -46,6 +117,8 @@ class CreatePoll extends React.Component{
                 InputLabelProps={{
                   shrink: true,
                 }}
+                value={this.state.startVotingDate || ''}
+                onChange={this.handleStartVotingDateChange}
             />
             <TextField
                 id="date"
@@ -56,6 +129,8 @@ class CreatePoll extends React.Component{
                 InputLabelProps={{
                   shrink: true,
                 }}
+                value={this.state.endVotingDate || ''}
+                onChange={this.handleEndVotingDateChange}
             />
             <Button variant="contained" color="primary" type="submit">
               Create poll
@@ -65,9 +140,6 @@ class CreatePoll extends React.Component{
     );
   }
 
-  createPoll() {
-    return alert("ciao");
-  }
 }
 
 export default CreatePoll;
